b9126e9332933aeec2aa124e6a8c6d50e53320aa
akio.tanaka <akio.tanaka@quietnan.xyz>
2023-01-27 15:23:51 +0900

    add UI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as BABYLON from 'babylonjs';
+import * as BABYLONGUI from 'babylonjs-gui';
 import _ from "lodash";
+
 
 export function testLodash(): void {
     const strs = [
@@ -41,4 +43,37 @@ export function testBabylon(): void {
     engine.runRenderLoop(() => {
         scene.render();
     });
+
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
+
+    // UI
+    const createButton = (
+        text: string,
+        callback?: (eventData: BABYLONGUI.Vector2WithInfo, eventState: BABYLON.EventState) => void): BABYLONGUI.Button => {
+        const button = BABYLONGUI.Button.CreateSimpleButton(text, text);
+        button.width = 0.2;
+        button.height = "40px";
+        button.color = "white";
+        button.background = "green";
+        button.horizontalAlignment = BABYLONGUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        if (callback) {
+            button.onPointerClickObservable.add(callback);
+        }
+        return button;
+    };
+
+    const advancedTexture = BABYLONGUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    const panel = new BABYLONGUI.StackPanel();    
+    advancedTexture.addControl(panel);
+    panel.addControl(createButton("Click Me 1", () => {
+        alert("press click me first");
+    }))
+    panel.addControl(createButton("Click Me 2", () => {
+        alert("press click me second");
+    }));
+    panel.addControl(createButton("Click Me 3", () => {
+        alert("press click me third");
+    }));
 }
